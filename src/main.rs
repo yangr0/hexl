12dc7531916bb6870ccf319b6b9c81624cd0cdf8
inc0gnit0 <iinc0gnit0@pm.me>
2020-03-10 19:21:51 -0500

    v0.2

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,21 @@ fn main() {
     let magenta = "\x1b[95m";
     let cyan = "\x1b[96m";
 
+    // Prompt
     println!("{}1. From Plain Text to hexidecimals\n", green);
     println!("{}2. From Hexidecimals to Plain text\n\n", blue);
     println!("{}hexl > ", red);
 
     let mut input = String::new(); // Creates new string
 
-    stdin().read_line(&mut input) // Reads the string
+    // Read input
+    stdin().read_line(&mut input)
         .ok()
-        .expect("Failed to read line"); // Error handling
+        .expect("Failed to read line");
 
     // Interpret input
     match &input.trim()[..] {
-        "1" => 
+        "1" => // Function goes here
+        "2" => // Function goes here
     };
-
 }
