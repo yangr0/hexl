0a6eb92e736cce9352b4f7da031b2325759c3db4
inc0gnit0 <iinc0gnit0@pm.me>
2020-03-10 20:51:13 -0500

    v0.3 - Need you to check over and fix the extra output on line 46

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,58 @@
 // Dependencies
 use std::io::stdin;
+use hex::encode;
+use hex::decode;
 
-// Main
-#[allow(unused_variables)]
-fn main() {
-    // Colors
-    let red = "\x1b[91m";
-    let green = "\x1b[92m";
-    let yellow = "\x1b[93m";
-    let blue = "\x1b[94m";
-    let magenta = "\x1b[95m";
-    let cyan = "\x1b[96m";
 
-    // Prompt
-    println!("{}1. From Plain Text to hexidecimals\n", green);
-    println!("{}2. From Hexidecimals to Plain text\n\n", blue);
-    println!("{}hexl > ", red);
+
+// Colors
+const RED: &str = "\x1b[91m";
+const GREEN: &str = "\x1b[92m";
+const YELLOW: &str = "\x1b[93m";
+const BLUE: &str = "\x1b[94m";
+const MAGENTA: &str = "\x1b[95m";
+
+
+
+// Encode
+fn p2h() {
+    println!("\n{}Plain Text String: ", YELLOW);
+
+    let mut input = String::new(); // Creates new string
+
+    // Read input
+    stdin().read_line(&mut input)
+        .ok()
+        .expect("Failed to read line");
+
+    println!("\n{}{}", GREEN, encode(input)); // Encode
+}
+
+
+
+// Decode
+fn h2p() {
+    println!("\n{}Hex String: ", BLUE);
+
+    let mut input = String::new(); // Creates new string
+
+    // Read input
+    stdin().read_line(&mut input)
+        .ok()
+        .expect("Failed to read line");
+
+    let hex = input.trim();
+
+    println!("\n{}{:?}", GREEN, decode(hex)); // Decode
+}
+
+
+
+// Prompt
+fn prompt() {
+    println!("{}1. From Plain Text to hexidecimals\n", YELLOW);
+    println!("{}2. From Hexidecimals to Plain text\n\n", BLUE);
+    println!("{}hexl > ", MAGENTA);
 
     let mut input = String::new(); // Creates new string
 
@@ -26,7 +63,17 @@ fn main() {
 
     // Interpret input
     match &input.trim()[..] {
-        "1" => // Function goes here
-        "2" => // Function goes here
+        "1" => p2h(),
+        "2" => h2p(),
+        _ => println!("{}Invalid Options!", RED),
     };
+}
+
+
+
+// Main
+#[allow(unused_variables)]
+fn main() {
+
+    prompt();
 }
