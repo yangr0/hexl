fe99811e0715e32f37f5937b7f7cbcb17bd5a632
inc0gnit0 <iinc0gnit0@pm.me>
2020-03-14 20:42:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ sudo apt-get install cargo
 cargo build
 
 input() {
-    read -n1 -p "Do you want this to be accessible anywhere(add to /bin)? [yn]" input
+    read -n1 -p "Do you want this to be accessible anywhere(add to /bin)? [y/n]" input
 
 case $input in  
 
-    y|Y) sudo mv target/debug/hexl /bin ;; 
+    y|Y) sudo mv target/debug/hexl /bin && echo "Use the command \"hexl\" to run" ;; 
     n|N) mv target/debug/hexl hexl ;; 
     *) echo "Invalid Options" && input ;;
 
